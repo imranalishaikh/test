d5e7181d527a2781d737e3e41f3f25059d3b0459
unknown <imranalishaikh92@yahoo.com>
2023-04-30 16:13:45 +0500

    update code

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const app = express();
 const dbConfig = require("./db");
 const roomsRoute = require("./routes/roomsRoute");
 
+app.use(express.json())
+
 app.use("/api/rooms", roomsRoute);
 
 const port = process.env.PORT || 5000;
